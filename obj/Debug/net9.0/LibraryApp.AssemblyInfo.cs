b40848b9a0da35ae159568a070106eab51e8a7ca
Nguyen Dinh Kien <hydinhkien@gmail.com>
2026-07-24 10:13:51 +0700

    menu admin

--- obj/Debug/net9.0/LibraryApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e1e6e6a369e6327bc3015024a5905ee3a1930b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90f736a8cd95f977175ebbaa69fb0954cf4d175")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
